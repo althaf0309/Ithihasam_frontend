cb00271b943a8fd2f04ea7aeec135271f088333a
althaf0309 <asharafsaleena22@gmail.com>
2026-05-04 18:02:30 +0530

    number chnage

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,5 +1,5 @@
-export const BUSINESS_PHONE = "+919567206406";
-export const BUSINESS_PHONE_DISPLAY = "+91 95672 06406";
-export const BUSINESS_WHATSAPP = "919567206406";
+export const BUSINESS_PHONE = "+919400096518";
+export const BUSINESS_PHONE_DISPLAY = "+91 94000 96518";
+export const BUSINESS_WHATSAPP = "919400096518";
 export const BUSINESS_EMAIL = "services@ithihasam.in";
 export const BUSINESS_LOCATION = "3rd Floor, Disha Building, Jubilee Road, Thalassery 670101";
