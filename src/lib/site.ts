baa2b05011adf8a352a1be0b1e4b2e6e4e86ddc7
althaf0309 <asharafsaleena22@gmail.com>
2026-05-24 22:41:34 +0530

    latest files and updates

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,4 +2,4 @@ export const BUSINESS_PHONE = "+919400096518";
 export const BUSINESS_PHONE_DISPLAY = "+91 94000 96518";
 export const BUSINESS_WHATSAPP = "919400096518";
 export const BUSINESS_EMAIL = "services@ithihasam.in";
-export const BUSINESS_LOCATION = "3rd Floor, Disha Building, Jubilee Road, Thalassery 670101";
+export const BUSINESS_LOCATION = "Anand Nivas, Venduttayi, Pinarayi 670649";
